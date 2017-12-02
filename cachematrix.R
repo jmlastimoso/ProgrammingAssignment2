e5b868159002a75d7c060a602ff197eb92bcbb6c
JMLastimoso <jllastimoso@up.edu.ph>
2017-12-02 16:50:16 +0800

    R Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Below are two functions that are used to create a special object that can 
+# cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+# makeCacheMatrix returns a list with a function to:
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the inverse of the matrix
+# 4. Get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+# cacheSolve computes the inverse of the matrix returned by makeCacheMatrix and cache the result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+        
 }
+
+# Test
+# x <- matrix(4:7, nrow=2, ncol=2)
+# x
+# mc <- makeCacheMatrix(x)
+# cs <- cacheSolve(mc)
+# cs
+
+# Returns a "getting cached data" message
+# cacheSolve(mc)
